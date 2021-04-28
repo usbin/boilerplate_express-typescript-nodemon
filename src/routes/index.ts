dbefe4c1f178b8015a3e598eac030fc7e58d054c
usbin <rooprete99@naver.com>
2021-04-28 15:09:04 +0900

    init

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { NextFunction } from "express";
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req: any, res: any, next: any) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
